f3b79e08f2856e00cf64cb8991c9ae441340221c
Sena <dimatesebas73@gmail.com>
2025-03-26 14:32:41 -0500

    .

--- Principal/obj/Debug/net8.0-windows/Principal.AssemblyInfo.cs
+++ Principal/obj/Debug/net8.0-windows/Principal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Principal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0703a154c5a04b459c28c8b136f71d83a47e1cbf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da57e05651bbe9889392cfb05ba13f0318125a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Principal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Principal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
