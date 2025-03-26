a25c0c5a286253de05799995ebca61c6b2e8b898
juan <alexzo8677@gmail.com>
2025-03-26 14:43:38 -0500

    act

--- Modelo/obj/Debug/net8.0/Modelo.AssemblyInfo.cs
+++ Modelo/obj/Debug/net8.0/Modelo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7da57e05651bbe9889392cfb05ba13f0318125a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3b79e08f2856e00cf64cb8991c9ae441340221c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modelo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
