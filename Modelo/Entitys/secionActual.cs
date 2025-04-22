f13cf0d962533c5b6e468969c41bd65e02ed2995
juan esteban grajales <alexzo8677@gmail.com>
2025-04-21 23:07:06 -0500

    actualizacion

---
+++ Modelo/Entitys/secionActual.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entitys
+{
+    public partial class secionActual
+    {
+        public static string Nombre { get; set; }
+        public static string Email { get; set; }
+        public static string Rol { get; set; }
+        public static int Id { get; set; }
+    }
+}
